650b963a9e9cbc5afb5924d0f174aad7b5f8849e
isthat0 <oc15@my.fsu.com>
2022-09-06 22:15:11 -0400

    Finish calculator. Change styling

--- java.js
+++ java.js
@@ -1,45 +1,72 @@
 const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.buttons');
+const deleteBtn = document.querySelector('.delete');
+const operators = document.querySelectorAll('.operators');
+const equal = document.querySelector('.equal');
+let displayValue = '';
+let firstValue = '';
+let currentOperator = '';
 
-function add (a, b){
-    return a + b;
-}
 
-function subtract (a, b){
-    return a - b;
-}
+//for each button clicked change the text content to match that
+buttons.forEach((button) => button.addEventListener('click', function(e){
+    numberClicked(e.target.textContent);
+    display.textContent = displayValue;
+}));
 
-function multiply (a, b){
-    return a * b;
-}
-
-function divide (a, b){
-    return a / b;
-}
-
-function operate (operator, num1, num2){
-    if(operator === '+'){
-        return add(num1,num2);
-    }
-    else if(operator === '-'){
-        return subtract(num1, num2);
-    }
-    else if(operator === '*'){
-        return multiply(num1, num2);
-    }
-    else if(operator === '/') {
-        return divide(num1,num2);
+//keep track of the numbers clicked
+function numberClicked(num){
+    if(displayValue.length <= 9){
+    displayValue += num;
     }
 }
+//for each operator clicked change the text content to match that
+operators.forEach((operator) => operator.addEventListener('click', function(e){
+    opClicked(e.target.textContent);
+    display.textContent = currentOperator;
+}));
 
-console.log(buttons);
-
-/*buttons.addEventListener('click',{
-
-})*/
-for (let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener('click', () => {
-        document.createTextNode(buttons[i]);
-        display.appendChild(buttons);
-    })
+//keep track of operator clicked and first value clicked
+function opClicked(operator){
+    firstValue= displayValue;
+    currentOperator = operator;
+    displayValue = ''; 
 }
+
+//do the math
+
+equal.addEventListener('click', function(){
+    calculation();
+    display.textContent = firstValue;
+})
+
+//calculate function. Probably could add it inside equal but ehh.
+function calculation(){
+    firstValue = parseFloat(firstValue);
+    displayValue = parseFloat(displayValue);
+    if(firstValue != '' && displayValue != ""){
+        if(currentOperator === "+"){
+            firstValue += displayValue;
+        }
+        else if(currentOperator === '-'){
+            firstValue -= displayValue;
+        }
+        else if(currentOperator === 'x'){
+            firstValue *= displayValue;
+        }
+        else if(currentOperator === '÷'){
+            firstValue /= displayValue;
+        }  
+    }
+}
+
+//clears the screen
+deleteBtn.addEventListener('click', () => {
+    firstValue= '';
+    operator = '';
+    displayValue= '';
+    display.textContent ='';
+})
+
+
+
